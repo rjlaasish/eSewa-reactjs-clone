d53ca8e8315353b3be72344786d27cdbf8b74f03
Aasish Rijal <rjlaasish@gmail.com>
2020-10-30 15:23:44 +0545

    -footer added

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -10,7 +10,7 @@ export default function ItemCard({cardImage,desc,offer,offerText}) {
         <div className="card-root">
             <img src={cardImage} alt="" className="card-img"/>
            {offer && <p className="optional-offer">{offerText}</p>} 
-            <h5 className="card-details" style={{color:"grey",height:"29px",overflow:"hidden"}}>{desc}</h5>
+            <h5 className="card-details" style={{color:"grey",width:"150px",height:"29px",textAlign:"center"}}>{desc}</h5>
             <div className="view-detail-btn">
                 <h5>View Details</h5>
                 
